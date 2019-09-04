e9e7b3cf5780a494accf88f6b5d8af2e183d04af
Hugh Cornett <cornetthugh@gmail.com>
2019-09-04 17:25:00 +0200

    changed checkForAnomalies to consider gravity removed main (was not being used) removed threshhold, windowsize constants

--- app/src/main/java/com/example/smartspacesproject1/averageAndFindAnomalies.java
+++ app/src/main/java/com/example/smartspacesproject1/averageAndFindAnomalies.java
@@ -4,8 +4,7 @@ import java.util.*;
 
 public class averageAndFindAnomalies
 {
-	public static final int WINDOW_SIZE = 5;
-	public static final Double TRESHHOLD = 2.0;
+	public static final Double GRAVITY = 9.81;
 
 	/**
 	   * Gets the moving average of a vector of data
@@ -19,7 +18,7 @@ public class averageAndFindAnomalies
 	public static Vector<Double> movingAverage(int windowSize, Vector<Double> data)
 	{
 		//create a new vector for the averaged data
-		Vector<Double> newData = new Vector<Double>();
+		Vector<Double> newData = new Vector<>();
 
 		//for every windowSize value (if windowSize is 5, for every 5th value)
 		for(int i = 0; i <= data.size() - windowSize; i += windowSize)
@@ -54,34 +53,13 @@ public class averageAndFindAnomalies
 		//for each value in the data
 		for(int i = 0; i < data.size(); i++)
 		{
-			//if the data is above the threshhold or below the negative threshhold
-			if(data.get(i) > threshhold || data.get(i) < -1 * threshhold)
+			//if the data is above the threshhold or below the negative threshhold (taking gravity into account)
+			if(data.get(i) > GRAVITY + threshhold || data.get(i) < GRAVITY + (-1 *  threshhold))
 			{
 				return true;
 			}
 		}
 		//if no data values were found to be anomalies
 		return false;
-	}
-
-	public static void main(String args[])
-	{
-		//initialise variables
-		Double[] dataArray = {0.0, -1.0, -0.1, -0.2, 0.0, 10.0, -0.4, -0.1, 0.3, -0.5, -0.1};
-		Vector<Double> data = new Vector<Double>(Arrays.asList(dataArray));
-		Vector<Double> averagedData = new Vector<Double>();
-		boolean anomaly;
-
-		//get moving average of data
-		averagedData = movingAverage(WINDOW_SIZE, data);
-		//check for anomalies
-		anomaly = checkForAnomalies(TRESHHOLD, averagedData);
-
-		//print results (original data, averaged data, if anomalies were found)
-		System.out.println("Original data: ");
-		System.out.println(data+"\n");
-		System.out.println("Averaged data: ");
-		System.out.println(averagedData+"\n");
-		System.out.println("Anomaly found? "+anomaly);
 	}
 }
